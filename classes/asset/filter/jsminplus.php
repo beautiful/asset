a3b92caae8129bf1319a69bab15db80ec65c1544
Luke Morton <lukemorton.designs@gmail.com>
2011-07-24 20:44:49 +0100

    Switch to using JsMinPlus.

--- classes/asset/filter/jsminplus.php
+++ classes/asset/filter/jsminplus.php
@@ -1,12 +1,12 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-class Asset_Filter_JsMin extends Asset_Filter {
+class Asset_Filter_JsMinPlus extends Asset_Filter {
 
 	protected $_types = array('js');
 	
 	public function __construct()
 	{
-		require_once '../../../vendor/jsmin-php/jsmin.php';
+		require_once dirname(__FILE__).'/../../../vendor/jsminplus/jsminplus.php';
 	}
 
 	protected function _filter(array $assets)
@@ -15,21 +15,14 @@ class Asset_Filter_JsMin extends Asset_Filter {
 		
 		foreach ($assets as $_asset)
 		{
-			$location = $_asset->location();
-			
-			$minified = JSMin::minify(
-				Request::factory($location)
-					->execute()
-					->body()
-			);
-			
-			$js_location = "js/{$this->_get_filename($location)}.min.js";
-			
+			$minified = JSMinPlus::minify($this->_asset_contents(
+				array($_asset)));
+			$filename = $this->_get_filename($_asset->location());
+			$js_location = "js/{$filename}.min.js";
 			file_put_contents(DOCROOT.$js_location, $minified);
 			
 			$final[] = new Asset_JS($js_location);
 		}
-		
 		
 		return $final;
 	}
