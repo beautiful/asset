2078b7bc41e99e79ad5665a60255df0b8c9e6747
Luke Morton <lukemorton.designs@gmail.com>
2012-03-14 08:53:28 +0000

    Change to single line comments in example config.

--- config/assets.php
+++ config/assets.php
@@ -2,28 +2,24 @@
 
 return array(
 
-	/**
-	 * Example Asset configuration:
-	 *
-	'groups' => array(
-		'stylesheets' => array(
-			array('css', 'css/screen.css'),
-			array('css', 'css/print.css', array(
-				'media' => 'print',
-			)),
-			array('less', 'css/test.less'),
-		),
-		'javascripts' => array(
-			array('js', 'http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.js'),
-			array('js', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.14/jquery-ui.js'),
-			array('js', 'js/app.js'),
-		),
-	),
+	// 'groups' => array(
+	// 	'stylesheets' => array(
+	// 		array('css', 'css/screen.css'),
+	// 		array('css', 'css/print.css', array(
+	// 			'media' => 'print',
+	// 		)),
+	// 		array('less', 'css/test.less'),
+	// 	),
+	// 	'javascripts' => array(
+	// 		array('js', 'http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.js'),
+	// 		array('js', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.14/jquery-ui.js'),
+	// 		array('js', 'js/app.js'),
+	// 	),
+	// ),
 	
-	'filters' => array(
-		'stylesheets' => array('Less', 'Concat'),
-		'javascripts' => array('Concat', 'JsMinPlus'),
-	),
-	 */
+	// 'filters' => array(
+	// 	'stylesheets' => array('Less', 'Concat'),
+	// 	'javascripts' => array('Concat', 'JsMinPlus'),
+	// ),
 	 
 );
